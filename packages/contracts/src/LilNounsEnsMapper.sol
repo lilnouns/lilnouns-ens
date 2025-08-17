182680f8fe5b7c892c5e040f3bf15090b045b186
Milad Nekofar <milad@nekofar.com>
2025-08-17 11:36:28 +0300

    feat(contracts): make `LilNounsEnsMapper` upgradeable
    Added support for upgradeable proxy deployment and initialization for `LilNounsEnsMapper`. Enhances flexibility and ensures forward compatibility with future updates.

--- packages/contracts/src/LilNounsEnsMapper.sol
+++ packages/contracts/src/LilNounsEnsMapper.sol
@@ -4,4 +4,28 @@ pragma solidity ^0.8.29;
 import { LilNounsEnsHolder } from "./LilNounsEnsHolder.sol";
 import { LilNounsEnsWrapper } from "./LilNounsEnsWrapper.sol";
 
-contract LilNounsEnsMapper is LilNounsEnsHolder, LilNounsEnsWrapper {}
+contract LilNounsEnsMapper is
+  LilNounsEnsHolder,
+  LilNounsEnsWrapper,
+  Initializable,
+  UUPSUpgradeable,
+  Ownable2StepUpgradeable
+{
+  /// @custom:oz-upgrades-unsafe-allow constructor
+  constructor() {
+    _disableInitializers();
+  }
+
+  /// @notice Initializes the contract
+  /// @dev Use this instead of the constructor when deploying with a proxy
+  function initialize(
+    address initialOwner,
+    IENS _ens,
+    IBaseRegistrar _baseRegistrar,
+    INameWrapper _nameWrapper
+  ) public initializer {
+    __LilNounsEnsVault_init(initialOwner);
+
+    __LilNounsEnsWrapper_init(ens, _baseRegistrar, _nameWrapper);
+  }
+}
