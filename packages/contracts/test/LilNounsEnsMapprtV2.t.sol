613b28adb480c9f26b31bd582683305fc6269bac
Milad Nekofar <milad@nekofar.com>
2025-08-18 22:12:05 +0300

    test(contracts): add skeleton test for `LilNounsEnsMapperV2`
    Introduce a basic test structure to ensure testability.
    
    This setup provides a foundation for future test cases, ensuring consistency and maintainability in contract testing.

---
+++ packages/contracts/test/LilNounsEnsMapprtV2.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.29;
+
+import { Test } from "forge-std/Test.sol";
+
+contract LilNounsEnsMapperV2Test is Test {
+  function test_Skeleton() public pure {
+    assertTrue(true);
+  }
+}
