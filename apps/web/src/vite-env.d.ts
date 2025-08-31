9b4bd1d2c66b1f6226c4d4b9e5ef3096a9b9d965
Milad Nekofar <milad@nekofar.com>
2025-08-31 08:21:40 +0300

    feat(web): extend `ImportMetaEnv` with custom env vars
    Facilitate access to custom environment variables in the app.
    
    Improves flexibility by enabling typed access to `VITE_CHAIN_ID`, `VITE_CHAIN_NAME`, and `VITE_WC_PROJECT_ID`.

--- apps/web/src/vite-env.d.ts
+++ apps/web/src/vite-env.d.ts
@@ -1,2 +1,12 @@
 /* eslint-disable unicorn/prevent-abbreviations */
 /// <reference types="vite/client" />
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
+
+interface ImportMetaEnv {
+  readonly VITE_CHAIN_ID?: string
+  readonly VITE_CHAIN_NAME?: string
+  readonly VITE_WC_PROJECT_ID?: string
+}
